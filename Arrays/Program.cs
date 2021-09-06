ded48f72e382e58f5119358fbb1878fd6f16d368
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-06 20:39:00 +0300

    First commit. Simple read write

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,28 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+            int[] arr = new int[5];
+
+            Console.WriteLine("Enter values");
+
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Entered values");
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Value; " + i + " is\t" + arr[i]);
+            }
+
+
+
         }
     }
 }
